c77407fe5fadd843ba26b193ab6bb443f3fdfff7
Kevin Freistroffer <kevin.freistroffer@gmail.com>
2019-11-25 20:44:28 -0800

    routes feed, post and /components, axios and react-router-dom

---
+++ src/routes/feed/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+class Feed extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div id='feed'></div>;
+  }
+}
+
+export default Feed;
+
+Feed.propTyes = {};
---
+++ src/routes/post/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div id='post'></div>;
+  }
+}
+
+export default Post;
+
+Post.propTyes = {};
